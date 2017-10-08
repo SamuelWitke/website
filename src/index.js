f43ee4b82a9074ca496b0e4a8f0bc585f1212472
SamuelWitke <SamuelWitke@gmail.com>
2017-10-08 16:31:24 -0400

    This should work for everyone

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Router from './router';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {Router, hashHistory } from 'react-router';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+
+
+import routes from './router';
 global.jQuery = require('jquery');
 require('bootstrap');
 
-ReactDOM.render(Router, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(
+  thunk
+)(createStore);
 
+ReactDOM.render(
+    <Router history={hashHistory} routes={routes} />
+, document.getElementById("root")
+);
 registerServiceWorker();
--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-
-
+import {Route,IndexRoute } from 'react-router';
 
 //Layouts
 import Home from './components/Home/Home.js';
@@ -9,10 +7,8 @@ import MainLayout from './components/Home/main-layout.js';
 //Pages
 import TicTacToe from './components/TicTacToe';
 export default (
-<Router history={browserHistory}>
-    <Route component={MainLayout}>
+<Route component={MainLayout}>
        	<Route path="/" component={Home} />
  		<Route path="TicTacToe"component={TicTacToe} />
-  	</Route>
-  </Router>
+</Route>
 );
